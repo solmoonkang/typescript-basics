15a73068a5088bd19637e880b57bb6f0778fb463
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-25 15:36:08 +0900

    feat: Add access modifier

---
+++ 07. access modifier/build/js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+class Post {
+    // public id: number = 0;
+    // public title: string = '';
+    // constructor(id: number, title: string) {
+    //     this.id = id;
+    //     this.title = title;
+    // }
+    // 위의 생성자를 다음과 같이 가독성 있게 구현할 수 있다.
+    constructor(id = 0, title = '') {
+        this.id = id;
+        this.title = title;
+    }
+    getPost() {
+        return `postId ${this.id}, postTitle ${this.title}`;
+    }
+}
+const post = new Post(1, "title 1");
+// PROTECTED
+class PostB extends Post {
+    getPost() {
+        return this.title;
+    }
+}
+console.log(post.id);
+console.log(post.title);
+// PRIVATE: 해당 클래스 내에서만 사용이 가능하다.
