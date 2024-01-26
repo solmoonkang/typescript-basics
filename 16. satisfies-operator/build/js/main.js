f1d79f0e77cbe06a74be1ab14e8d4bbf6ff30c94
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-26 12:44:09 +0900

    feat: Add satisfies operator

---
+++ 16. satisfies-operator/build/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// SATISFIES OPERATOR
+const palette = {
+    red: [255, 0, 0],
+    green: '#00ff00',
+    // bleu: [0, 0, 255]   // Error가 발생했다고 바로 알려준다.
+    blue: [0, 0, 255]
+    // platypus: false;   // 지정한 타입 외의 타입을 지정해서 Error가 발생한다.
+};
+const redComponent = palette.red.at(0); // at을 사용할 수 있다는 것은 red가 배열이라는 의미이다.
+const greenComponent = palette.green.toUpperCase(); // string | RGB 형식으로 지정했기 때문에 toUpperCase 메서드를 사용하지 못한다.
